644d92a36c299a2b084e96993ad8c1b7f3e9bb2d
youxianyen <youxianyen@sina.com>
2025-07-18 08:01:14 +0000

    Update gift system migration files and controller

--- Modules/Gift/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('gift::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('gift.name') !!}
-    </p>
-@endsection
